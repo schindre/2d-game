b26cce3854ed45e49b4454537d0b88365d3c8c9f
schindre <32387064+schindre@users.noreply.github.com>
2018-02-15 10:32:14 +0100

    Initial commit

---
+++ Assets/Scripts/GameControl.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameControl : MonoBehaviour {
+
+	public static GameControl instance;
+	public GameObject GameOverText;
+	public Text scoreText;
+	public bool GameOver = false;
+	public float scrollSpeed = -1.5f;
+
+	private int score = 0;
+
+	// Use this for initialization
+	void Awake ()
+	{
+		if (instance == null) { 
+			instance = this;
+		} else if (instance != this)
+		{
+			Destroy(gameObject);
+		}
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+		if (GameOver == true && Input.GetMouseButtonDown (0))
+
+
+		{
+			SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex);
+			SoundMananger.PlaySound ("restart");
+
+		}	
+	}
+
+	public void birdScored()
+	{
+		if (GameOver) 
+		{
+			return;
+
+		}
+		score++;
+		scoreText.text = "Score: " + score.ToString ();
+		SoundMananger.PlaySound ("point");
+	}
+	public void BirdDied()
+	{
+		GameOverText.SetActive (true);
+		GameOver = true;
+		SoundMananger.PlaySound ("death");
+	}
+
+}
---
+++ Assets/Scripts/SoundMananger.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundMananger : MonoBehaviour {
+
+	public static AudioClip deathSound, jumpSound, restartSound, pointSound;
+	static AudioSource AudioSrc;
+
+	// Use this for initialization
+	void Start () {
+
+		deathSound = Resources.Load<AudioClip> ("death");
+		jumpSound = Resources.Load<AudioClip> ("jump");
+		restartSound = Resources.Load<AudioClip> ("restart");
+		pointSound = Resources.Load<AudioClip> ("point");
+
+		AudioSrc = GetComponent<AudioSource> ();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public static void PlaySound (string clip)
+	{
+		
+		switch (clip) {	
+		case "death":
+			AudioSrc.PlayOneShot (deathSound);
+			break;
+		case "jump":
+			AudioSrc.PlayOneShot (jumpSound);	
+			break;
+		case "restart":
+			AudioSrc.PlayOneShot (deathSound);
+			break;
+		case "point":
+			AudioSrc.PlayOneShot (pointSound);
+			break;
+		}
+	}
+}
